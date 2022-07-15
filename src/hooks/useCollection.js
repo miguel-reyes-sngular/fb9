6c27c4fb67319558135414a2cbe9db5b80a02b45
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-15 16:53:11 -0500

    Clase 196

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+
+import { collection, onSnapshot } from 'firebase/firestore'
+
+export const useCollection = (col) => {
+  const [documents, setDocuments] = useState(null)
+
+  useEffect(() => {
+    let ref = collection(db, col)
+
+    const unsub = onSnapshot(ref, (snapshot) => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({
+          id: doc.id,
+          ...doc.data()
+        })
+      })
+      setDocuments(results)
+    })
+
+    return () => unsub()
+
+  }, [col])
+
+  return { documents }
+}
