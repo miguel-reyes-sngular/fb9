babf9a05bf2fcb518d70ef652364a06bedbbb834
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-15 17:01:10 -0500

    Clase 197

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 
+import { db } from '../firebase/config'
+import { collection, addDoc } from 'firebase/firestore'
+
 export const BookForm = () => {
   const [newBook, setNewBook] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(newBook)
+
+    const ref = collection(db, 'books')
+
+    await addDoc(ref, {
+      title: newBook
+    })
 
     setNewBook('')
   }
