130fc32cc343600f77644814f8ea2a6647576a54
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-18 09:19:08 -0500

    Clase 202

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 export const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { error, login } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)
+    login(email, password)
   }
 
   return (
@@ -34,6 +36,7 @@ export const Login = () => {
           />
         </label>
         <button>log in</button>
+        {error && <p>{error}</p>}
       </form>
     </div>
   )
